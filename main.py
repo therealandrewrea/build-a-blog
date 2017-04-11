c5ae97e60b6bb2f715b461d0a2f632e9f4e5001a
Andy Rea <therealandrewrea@gmail.com>
2017-04-10 19:41:31 -0500

    formatted main body and added comments for guidance

--- main.py
+++ main.py
@@ -15,11 +15,28 @@
 # limitations under the License.
 #
 import webapp2
+import jinja
+import os
+from google.appengine.txt import db
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
+class Index(webapp2.RequestHandler):
+
+    #base page, (redirect to blog to view recent posts)
+
+class NewPost(webapp2.RequestHandler):
+    def get(self): #use thin newpost template -
+
+    def post(self): #check and "create" post with a permalink
+
+class Posts(webapp2.RequestHandler):  #database of previous and newly added posts
+    title = db.StringProperty(Required=True)
+    post_body = db.TextProperty(Required=True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def get(self, id): #pulling with ID reference to database - use post template
         self.response.write('Hello world!')
 
-app = webapp2.WSGIApplication([
+app = webapp2.WSGIApplication([ #contains unique routing in LC101 directions as well as a corresponding handler for each class above
     ('/', MainHandler)
 ], debug=True)
